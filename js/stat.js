8a895f4fd1c97697fd7b94e85ed709ecc02ad865
Daria <dasha_kravchenko_00@mail.ru>
2020-09-21 14:07:31 +0300

    added fixes acc to linter

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,8 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
-var CLOUD_Y = 10;
 var GAP = 50;
 var BAR_WIDTH = 40;
 var FONT_Y = 270;
@@ -12,7 +13,7 @@ var renderCloud = (ctx, x, y, color) => {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(elements) {
+var getMaxElement = function (elements) {
   var maxElement = elements[0];
 
   for (var i = 1; i < elements.length; i++) {
@@ -25,9 +26,9 @@ var getMaxElement = function(elements) {
 
 var getRandomNumber = (min, max) => {
   return Math.floor(min + Math.random() * (max + 1 - min));
-}
+};
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
@@ -42,14 +43,11 @@ window.renderStatistics = function(ctx, names, times) {
     var ELEMENT_X = CLOUD_X + GAP + (BAR_WIDTH + GAP) * i;
     var saturation = getRandomNumber(1, 100);
 
-    names[i] === 'Вы'
-    ? ctx.fillStyle = 'rgb(255, 0, 0)'
-    : ctx.fillStyle = 'hsl(240, ' + saturation + '% , 50%)';
-
+    names[i] === 'Вы' ? ctx.fillStyle = 'rgb(255, 0, 0)' : ctx.fillStyle = 'hsl(240, ' + saturation + '% , 50%)';
     ctx.fillRect(ELEMENT_X, 100 + (MAX_BAR_HEIGHT - BAR_HEIGHT), BAR_WIDTH, BAR_HEIGHT);
 
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), ELEMENT_X, 90 + (MAX_BAR_HEIGHT - BAR_HEIGHT), BAR_WIDTH, BAR_HEIGHT)
+    ctx.fillText(Math.round(times[i]), ELEMENT_X, 90 + (MAX_BAR_HEIGHT - BAR_HEIGHT), BAR_WIDTH, BAR_HEIGHT);
     ctx.fillText(names[i], ELEMENT_X, FONT_Y);
   }
-}
+};
